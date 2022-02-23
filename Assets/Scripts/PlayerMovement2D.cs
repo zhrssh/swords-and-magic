8dcd0084a62efd1c83635f131be84a96879b1bda
Zherish <98146850+zhrssh@users.noreply.github.com>
2022-02-23 19:06:45 +0800

    Added Animations and Starter Armor (Not finished yet)

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -8,18 +8,45 @@ public class PlayerMovement2D : MonoBehaviour
 
     // system
     Vector2 movement;
+    bool isFacingRight = true;
 
-    // inputs
 
     // references
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        // if moving left
+        if (movement.x < 0 && isFacingRight)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            isFacingRight = false;
+        }
+
+
+        // if moving right
+        if (movement.x > 0 && !isFacingRight)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            isFacingRight = true;
+        }
+
         movement.Normalize();
+
+        // Animation
+        if (movement.magnitude > 0)
+        {
+            animator.SetBool("Is Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Is Moving", false);
+        }
+
     }
 
     private void FixedUpdate()
